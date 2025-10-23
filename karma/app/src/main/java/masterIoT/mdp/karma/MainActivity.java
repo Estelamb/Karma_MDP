e5c279e4ef463de9e3d8e4c45067427e8ceeb349
JavierGrimaldos <javiergrimaldoschavarria@gmail.com>
2025-10-23 12:44:01 +0200

    Proyecto Android Studio

---
+++ karma/app/src/main/java/masterIoT/mdp/karma/MainActivity.java
@@ -0,0 +1,55 @@
+package masterIoT.mdp.karma;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.os.Handler;
+import android.widget.TextView;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mensajesMotivados;
+    private Handler handler;
+    private Runnable runnable;
+    private List<String> mensajes;
+    private Random random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mensajesMotivados=findViewById(R.id.txvwMensajesPositivos);
+        mensajes = Arrays.asList("Believe in yourself and all that you are.", "Every day is a new beginning.", "You are stronger than you think.", "Dream big and dare to fail.", "Keep going — you’re doing great!", "The best time for new beginnings is now.", "Be the energy you want to attract.", "Your only limit is your mind.", "Small steps every day lead to big results.", "Stay positive, work hard, make it happen.");
+        random= new Random();
+        handler= new Handler();
+        runnable= new Runnable(){
+            @Override
+            public void run(){
+                String mensaje=mensajes.get(random.nextInt((mensajes.size())));
+                mensajesMotivados.setText(mensaje);
+                handler.postDelayed(this, 10000);
+            }
+        };
+        handler.post(runnable);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Detiene el handler cuando la actividad se destruye
+        handler.removeCallbacks(runnable);
+    }
+}
